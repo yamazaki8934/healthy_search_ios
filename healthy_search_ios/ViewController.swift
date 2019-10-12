b1fd3f3d2de787132ccf4fae7430fa1870a8d76b
yamazaki <jdmvgaj@gmail.com>
2019-10-12 19:46:06 +0900

    first commit

---
+++ healthy_search_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  healthy_search_ios
+//
+//  Created by 山崎浩毅 on 2019/10/12.
+//  Copyright © 2019 山崎浩毅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
